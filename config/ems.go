ccf9bff5225cd7aa1de53dca1382d07e4619805d
mlecherf <mathis.lecherf@gmail.com>
2024-01-07 16:44:58 +0100

    Feat(core): Integrate Zerolog

--- config/ems.go
+++ config/ems.go
@@ -10,4 +10,5 @@ This package defines how is structured the complete config file
 type EMS struct {
 	Equipments []Equipment           `json:"equipments"`
 	Contexts   context.Configuration `json:"context"`
+	Debug      bool                  `json:"debug"`
 }
