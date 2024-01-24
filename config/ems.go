762bdaa9d7206fd3f2a1d2c47b965b0ef86209c4
kilianp07 <57315153+kilianp07@users.noreply.github.com>
2024-01-24 09:34:49 +0100

    Feat: Integrate Context (#17)
    Co-authored-by: TMEIGNIER <tanguy.meignier@ynov.com>

--- config/ems.go
+++ config/ems.go
@@ -1,9 +1,13 @@
 package config
 
+import (
+	context "github.com/EcoPowerHub/context/pkg"
+)
+
 /*
 This package defines how is structured the complete config file
 */
-
 type EMS struct {
-	Equipments []Equipment `json:"equipments"`
+	Equipments []Equipment           `json:"equipments"`
+	Contexts   context.Configuration `json:"context"`
 }
