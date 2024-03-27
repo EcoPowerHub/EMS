7b639e889de1bd8514a8567fcb64add57c54a99b
kilianp07 <kilianp07@icloud.com>
2024-03-27 11:42:31 +0100

    Feat(triposter): integrate library

--- config/ems.go
+++ config/ems.go
@@ -3,15 +3,17 @@ package config
 import (
 	"github.com/EcoPowerHub/EMS/services/modes"
 	context "github.com/EcoPowerHub/context/pkg"
+	triposter "github.com/EcoPowerHub/triposter/pkg"
 )
 
 /*
 This package defines how is structured the complete config file
 */
 type EMS struct {
-	Equipments []Equipment           `json:"equipments"`
-	Contexts   context.Configuration `json:"context"`
-	Services   map[string]Service    `json:"services"`
-	Modes      modes.Conf            `json:"modes"`
-	Debug      bool                  `json:"debug"`
+	Equipments []Equipment             `json:"equipments"`
+	Contexts   context.Configuration   `json:"context"`
+	Services   map[string]Service      `json:"services"`
+	Modes      modes.Conf              `json:"modes"`
+	Debug      bool                    `json:"debug"`
+	Triposter  triposter.Configuration `json:"triposter"`
 }
--- go.mod
+++ go.mod
@@ -1,12 +1,13 @@
 module github.com/EcoPowerHub/EMS
 
-go 1.21
+go 1.21.5
 
-toolchain go1.21.3
+toolchain go1.22.1
 
 require (
 	github.com/EcoPowerHub/context v0.1.5
-	github.com/EcoPowerHub/shared v0.1.5
+	github.com/EcoPowerHub/shared v0.1.6
+	github.com/EcoPowerHub/triposter v0.1.0
 	github.com/PaesslerAG/gval v1.2.2
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/rs/zerolog v1.32.0
@@ -24,5 +25,5 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
